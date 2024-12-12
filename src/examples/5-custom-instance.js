19e1a732f129f203d25a4ebb8783e5e11aff671e
Iga Tunde <igatunde06@gmail.com>
2024-12-12 20:56:56 +0100

    custom instance

--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,16 +1,22 @@
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect } from "react";
 
-const randomUserUrl = 'https://randomuser.me/api';
+import authFetch from "../axios/custom";
+
+const randomUserUrl = "https://randomuser.me/api";
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+    try {
+      const response = await authFetch("/react-store-product");
+      const response2 = await axios(randomUserUrl);
+    } catch (error) {}
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>custom instance</h2>;
+  return <h2 className="text-center">custom instance</h2>;
 };
 export default CustomInstance;
